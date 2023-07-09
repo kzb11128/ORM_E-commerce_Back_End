bbbc5c97bef9e6e6ddd2a814b6fb586c55cbca65
Zhang <kzb11128@gmail.com>
2023-07-09 11:43:04 -0400

    minor updates to models nd routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,9 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -19,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{model: Product, as: 'category_product'}]
+      include: [{model: Product}]
     });
 
     if (!categoryData){
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
     const productData = await Product.findAll({
       include: 
       [
-        {model: Category, as: 'product_category'}, 
+        {model: Category}, 
         {model: Tag, through: ProductTag, as: 'product_tag'}
       ]
     });
@@ -29,7 +29,7 @@ router.get('/:id', async (req, res) => {
     const productData = await Product.findByPk(req.params.id, {
       include: 
       [
-        {model: Category, as: 'product_category'}, 
+        {model: Category}, 
         {model: Tag, through: ProductTag, as: 'product_tag'}
       ]
     });
